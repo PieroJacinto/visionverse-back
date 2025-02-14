7e700c65c101d5662cf13928f22a1c1eb7c0cf51
PieroJacinto <pierojacinto@gmail.com>
2025-02-14 11:49:09 -0300

    acces granted

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,18 +1,18 @@
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import passport from 'passport';
-import * as process from 'process';
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import passport from "passport";
+import * as process from "process";
 
 export const configurePassport = () => {
-  const isProduction = process.env.NODE_ENV === 'production';
-  const callbackURL = isProduction 
-    ? 'https://visionverse-back.vercel.app/api/auth/google/callback'
-    : 'http://localhost:3000/api/auth/google/callback';
+  const isProduction = process.env.NODE_ENV === "production";
+  const callbackURL = isProduction
+    ? "https://visionverse-back.vercel.app/api/auth/google/callback"
+    : "http://localhost:3000/api/auth/google/callback";
 
   passport.serializeUser((user, done) => {
     done(null, {
       id: user.id,
       email: user.emails[0].value,
-      displayName: user.displayName
+      displayName: user.displayName,
     });
   });
 
@@ -20,21 +20,23 @@ export const configurePassport = () => {
     done(null, user);
   });
 
-  passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: callbackURL,
-    proxy: true
-  },
-  function(accessToken, refreshToken, profile, done) {
-    console.log('Google Strategy executing with callback URL:', callbackURL);
-    console.log('Email received:', profile.emails[0].value);
-    
-    const userEmail = profile.emails[0].value;
-    if (userEmail === 'pierojacinto@gmail.com') {
-      return done(null, profile);
-    } else {
-      return done(null, false, { message: 'Email no autorizado' });
-    }
-  }));
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: callbackURL,
+        proxy: true,
+      },
+      function (accessToken, refreshToken, profile, done) {
+        console.log(
+          "Google Strategy executing with callback URL:",
+          callbackURL
+        );
+        console.log("Email received:", profile.emails[0].value);
+
+        return done(null, profile); // Permite cualquier email
+      }
+    )
+  );
 };
